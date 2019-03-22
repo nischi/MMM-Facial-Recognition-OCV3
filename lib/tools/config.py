805096f9be58eb4e316df652cd87d1ccbade3f64
Thierry Nischelwitzer <nischi@nischi.ch>
2019-03-22 12:18:33 +0100

    add new data and persons

--- lib/tools/config.py
+++ lib/tools/config.py
@@ -29,7 +29,7 @@ class ToolsConfig (CommonConfig):
     captureName = 'thierry'
 
 
-    USERS = ['christina', 'leonas', 'thierry', 'larina', 'ilana']
+    USERS = ['elisabeth', 'christina', 'leonas', 'thierry', 'sepp', 'bruno', 'larina', 'ilana', 'stefan', 'josef']
 
     if ('FACE_USERS' in os.environ):
         u = os.environ['FACE_USERS']
